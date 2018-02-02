32cba71bdc01f7a5516a88d5ada07f6ddbf6318f
SunshineM <zhanglm414@gmail.com>
2018-02-02 20:57:48 +0800

    first commit

---
+++ app/components/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/global.css';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="content">
+        Hello Parcel&React!!!
+      </div>
+    );
+  }
+}
+
+export default Hello;
